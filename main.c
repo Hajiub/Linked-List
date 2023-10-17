02575dbe376a2fb98ba9711df5c35e05866ca4f2
Abraham <brahimaitelmahjoub12345@gmail.com>
2023-10-17 17:57:24 +0100

    added reverse and len

--- main.c
+++ main.c
@@ -1,51 +1,104 @@
-// version 1.0.0
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 #include <assert.h>
 
 typedef struct node
 {
-    int value;
-    struct node *next;
+    int val;
+    struct node* next;
 } node;
+
+node* createNode(int val)
+{
+    node* n = (node*)malloc(sizeof(node));
+    assert(n != NULL);
+    n->val = val;
+    n->next = NULL;
+    return n;
+}
+
+void insertNode(node** head, int val)
+{
+    node* n = createNode(val);
+    if (*head == NULL)
+    {
+        *head = n;
+    }
+    else
+    {
+        node* temp = *head;
+        while (temp->next)
+        {
+            temp = temp->next;
+        }
+        temp->next = n;
+    }
+}
+
+void reverse(node** head)
+{
+    node* current, *prev, *next;
+    current = *head;
+    prev = NULL;
+    while (current)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *head = prev;
+}
+
+void display(node* head)
+{
+    node* current = head;
+    while (current)
+    {
+        printf("%i -> ", current->val);
+        current = current->next;
+    }
+    printf("None \n");
+}
+
+int len(node *head)
+{
+    node *list = head;
+    int len = 0;
+    while (list)
+    {
+        list = list->next;
+        len++;
+    }
+    return len;
+}
+
+void freeList(node* head)
+{
+    node* current = head;
+    node* tmp;
+    while (current)
+    {
+        tmp = current->next;
+        free(current);
+        current = tmp;
+    }
+}
 
 int main()
 {
-    node *n, *list;
-    list = NULL;
-    n = (node *)malloc(sizeof(node));
-    if (n == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed.\n");
-        return 1; 
-    }
-    n->value = 1;
-    n->next = NULL;
-    list = n;
+    node* my_list = NULL;
 
-    n = (node *)malloc(sizeof(node));
-    if (n == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed.\n");
-        return 1; 
-    }
-    n->value = 2;
-    n->next = NULL;
-    list->next = n;
+    insertNode(&my_list, 1);
+    insertNode(&my_list, 2);
+    insertNode(&my_list, 3);
+    printf("%i\n", len(my_list));
+    reverse(&my_list);
 
-    for (node *current = list; current != NULL; current = current->next)
-    {
-        printf("%i\n", current->value);
-    }
+    printf("Linked List: ");
+    display(my_list);
 
-    node *tmp;
-    while (list != NULL)
-    {
-        tmp = list;
-        list = list->next;
-        free(tmp);
-    }
+    freeList(my_list);
+
     return 0;
 }
